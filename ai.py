360b785e356fdab14e24f89916161f2193a864f5
Kevin Bishop <kevinandrewbishop@gmail.com>
2017-01-30 18:32:15 -0500

    created check_legal_command method for AI

--- ai.py
+++ ai.py
@@ -15,14 +15,35 @@ This will have to somehow involve modeling opponent moves. A naive start would
 be to assume opponent makes random choices mixed with simplistic heuristics.
 '''
 from config import NUM_ACTIONS, VALID_ACTIONS
+from user import User
 
-class AI():
-    def __init__(self):
-        pass
+class AI(User):
+    def check_legal_command(self, command, params = None):
+        #returns True if command is legal
+        if command in ['rest', 'shield']:
+            return True
+        if command == 'shoot':
+            #params must be x, y tuple of target
+            tile = self.world.check_tile(*params)
 
-    def check_legal_move(self, move):
-        #returns True if move is legal
-        pass
+        if command == 'move':
+            #params must be (x, y, direction)
+            x, y, direction = params
+            if direction == 'u':
+                x -= 1
+            if direction == 'd':
+                x += 1
+            if direction == 'r':
+                y += 1
+            if direction == 'l':
+                y -= 1
+            tile = self.world.check_tile(x, y)
+
+        if tile == 'NA':
+            return False
+        else:
+            return True
+
 
     def evaluate_state(self, state):
         #scores the game state. Higher scores are more advantageous
@@ -36,3 +57,16 @@ class AI():
 
     def build_tree(self):
         pass
+
+
+
+if __name__ == '__main__':
+    from world import World
+    barriers = [(2,2),(2,3),(2,4),(2,5),(3,2),(4,2),(5,2)]
+    barriers += [(9-item[0],14-item[1]) for item in barriers]
+    world = World((10,15))
+    for b in barriers:
+        x, y = b
+        world.tiles[x][y] = 'X'
+    user1 = AI(world, 1)
+    user2 = User(world, 2)
